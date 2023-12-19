b443e2b52c72face2f322a52e72f58bc478a2f01
Jennifer UBALDO <jubaldo@bess-f1r3s8.clusters.42paris.fr>
2023-12-19 10:53:18 +0100

    .h

--- minishell.h
+++ minishell.h
@@ -6,11 +6,20 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 21:59:31 by jubaldo           #+#    #+#             */
-/*   Updated: 2023/12/14 22:01:16 by jubaldo          ###   ########.fr       */
+/*   Updated: 2023/12/19 10:52:00 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
 
 #endif
