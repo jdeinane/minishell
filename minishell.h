f7b1c1123892c4f6480f9a4f1ca459c874087843
Jennifer Ubaldo <jenniferdianeu@gmail.com>
2023-12-20 23:41:56 +0100

    h

--- minishell.h
+++ minishell.h
@@ -6,19 +6,22 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 21:59:31 by jubaldo           #+#    #+#             */
-/*   Updated: 2023/12/19 19:22:23 by jubaldo          ###   ########.fr       */
+/*   Updated: 2023/12/20 23:41:43 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+# include <stdio.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/types.h>
 
 
 
