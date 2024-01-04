af7f286a43341cc5a73ebd04e3ae99dc9ffca4a4
Jennifer UBALDO <jubaldo@bess-f1r3s11.clusters.42paris.fr>
2024-01-04 13:52:13 +0100

    token file ok

--- srcs/token/token_input.c
+++ srcs/token/token_input.c
@@ -6,59 +6,58 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 12:52:11 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/01/04 11:54:45 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/01/04 13:51:16 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	**expand_tokens_array(char **tokens, int size)
+void	add_token(t_tokenizer *tokenizer, int token_len)
 {
-	char	**new_tokens;
-	int		i;
-
-	new_tokens = malloc(sizeof(char *) * size);
-	i = 0;
-	while (i < size / 2)
+	if (tokenizer->count >= tokenizer->size - 1)
 	{
-		new_tokens[i] = tokens[i];
+		tokenizer->size *= 2;
+		tokenizer->tokens = expand_tokens_array(tokenizer->tokens, \
+		tokenizer->size);
+	}
+	tokenizer->tokens[tokenizer->count] = ft_strndup(tokenizer->start, \
+	token_len);
+	tokenizer->tokens[++tokenizer->count] = NULL;
+}
+
+void	update_quote_status(char c, t_tokenizer *tokenizer)
+{
+	if ((c == '"' || c == '\'') && !tokenizer->in_quote)
+	{
+		tokenizer->in_quote = true;
+		tokenizer->current_quote = c;
+	}
+	else if (c == tokenizer->current_quote && tokenizer->in_quote)
+		tokenizer->in_quote = false;
+}
+
+void	check_and_add_token(t_tokenizer *tokenizer, char c, int pos)
+{
+	if ((c == ' ' || c == '\n' || c == '\t') && !tokenizer->in_quote
+		&& pos > (tokenizer->start - tokenizer->tokens[0]))
+	{
+		add_token(tokenizer, pos - (tokenizer->start - tokenizer->tokens[0]));
+		tokenizer->start = tokenizer->tokens[0] + pos + 1;
+	}
+}
+
+void	tokenize_input(t_tokenizer *tokenizer, char *input)
+{
+	int	i;
+
+	i = 0;
+	tokenizer->start = input;
+	while (input[i] != '\0')
+	{
+		update_quote_status(input[i], tokenizer);
+		check_and_add_token(tokenizer, input[i], i);
 		i++;
 	}
-	free(tokens);
-	return (new_tokens);
-}
-
-void	add_token(char **tokens, int *count, char *start, int len)
-{
-	tokens[*count] = ft_strndup(start, len);
-	(*count)++;
-	tokens[*count] = NULL;
-}
-
-void	handle_quotes(char c, bool *in_quote, char *current_quote)
-{
-	if ((c == '"' || c == '\'') && !*in_quote)
-	{
-		*in_quote= true;
-		*current_quote = c;
-	}
-	else if (c == *current_quote && *in_quote)
-		*in_quote = false;
-}
-
-void	process_token(char **tokens, int *count, char **start, char *end)
-{
-	if (end > *start)
-	{
-		add_token(tokens, count, *start, end - *start);
-		*start = end + 1;
-	}
-}
-
-char	**tokenize_input(char *input)
-{
-	int		count;
-	int		i;
-	int		len;
-	char	**tokens;
+	if (tokenizer->start != input + i)
+		add_token(tokenizer, input + i - tokenizer->start);
 }
