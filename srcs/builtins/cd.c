5e0eb717eb1c470050a8680ad9ebbba3265518e0
Jennifer Ubaldo <jenniferdianeu@gmail.com>
2024-01-25 23:59:34 +0100

    better cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,45 +6,64 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 18:39:13 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/01/23 18:58:47 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/01/25 23:59:29 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-int	builtin_cd(char **av, char **envp)
+static int	change_directory(t_data *data, char *path)
 {
-	char	*old_path;
-	char	*new_path;
-	char	*current_path;
+	char	*path_pwd;
+	char	buf[MAX_PATH];
 
-	old_path = getcwd(NULL, 0);
-	if (!old_path)
-		return (perror("cd: error retrieving current directory"), 1);
-	if (av[1] == NULL || ft_strcmp(av[1], "~") == 0)
-		new_path = get_env_var(envp, "HOME");
-	else if (ft_strcmp(av[1], "-") == 0)
-		new_path = get_env_var(envp, "OLDPWD");
+	if (chdir(path) != 0)
+		return (error_msg("cd", path, strerror(errno), 1));
+	getcwd(buf, MAX_PATH);
+	update_work_dir_var(data, path_pwd);
+	return (EXIT_SUCCESS);
+}
+
+static void	update_work_dir_var(t_data *data, char *path)
+{
+	set_env_var(data, OLD_PWD, get_env_var_value(data->env, PWD));
+	set_env_var(data, PWD, path);
+	if (data->old_work_dir)
+	{
+		free_ptr(data->old_work_dir);
+		data->old_work_dir = ft_strdup(data->work_dir);
+	}
+	if (data->work_dir)
+	{
+		free_ptr(data->work_dir);
+		data->work_dir = ft_strdup(path);
+	}
+	free(path);
+}
+
+int	builtin_cd(t_data *data, t_commands *cmds, int i)
+{
+	char	*path;
+
+	if (!cmds->cmd[i].args[1] || is_space(cmds->cmd[i].args[1][0])
+			|| cmds->cmd[i].args[1][0] == '\0'
+			|| ft_strncmp(cmds->cmd[i].args[1], "--", 3) == 0
+			|| ft_strncmp(cmds->cmd[i].args[1], "~", 2) == 0)
+	{
+		path = get_env_var_value(data->env, "HOME");
+		if (!path || *path == '\0' || is_space(*path))
+			return (error_msg("cd", NULL, "HOME not set", EXIT_FAILURE));
+		return (change_directory(data, path));
+	}
+	else if (cmds->cmd[i].args[2])
+		return (error_msg("cd", NULL, "too many arguments", EXIT_FAILURE));
+	else if (ft_strncmp(cmds->cmd[i].args[1][0] == '-'))
+	{
+		path = get_env_var_value(data->env, OLD_PWD);
+		if (!path)
+			return (error_msg("cd", NULL, "OLDPWD not set", EXIT_FAILURE));
+		return (change_directory(data, path));
+	}
 	else
-		new_path = av[1];
-	if (!new_path)
-	{
-		perror("cd: error retrieving new directory");
-		free(old_path);
-		return (1);
-	}
-	if (chdir(new_path) != 0)
-	{
-		perror("cd");
-		free(old_path);
-		return (1);
-	}
-	set_env_var(envp, "OLDPWD", old_path);
-	free(old_path);
-	current_path = getcwd(NULL, 0);
-	if (!current_path)
-		return (perror("cd: error updating PWD"), 1);
-	set_env_var(envp, "PWD", current_path);
-	free(current_path);
-	return (0);
+		return (change_directory(data, cmds->cmd[i].args[1]));
 }
