d64d171ae5fb0bd32843bb1f84551057c47ccf04
Jennifer UBALDO <jubaldo@bess-f2r1s9.clusters.42paris.fr>
2024-01-28 21:21:49 +0100

    lexer new

--- srcs/lexer/process_input.c
+++ srcs/lexer/process_input.c
@@ -6,9 +6,27 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 20:43:52 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/01/28 20:43:57 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/01/28 21:20:53 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+void	process_input(char *input, t_data *data)
+{
+	int (index) = 0;
+	t_list (*tokens) = NULL;
+	while (input[index])
+	{
+		if (is_space(input[index]))
+			index++;
+		else if (input[index] == '\'' || input[index] == '"')
+			handle_quotes(&input, &index, &tokens);
+		else if (ft_strchr("<>|;", input[index]) != NULL)
+			handle_redir_and_pipes(&input, &index, &tokens);
+		else if (input[index] == '$')
+			handle_env_var(&input, &index, &tokens);
+		else
+			process_token(data, &input, &index, &tokens);
+	}
+}
