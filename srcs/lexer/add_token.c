4e7badd2abb8768b70945e31dcafefb021caf6aa
Jennifer Ubaldo <jenniferdianeu@gmail.com>
2024-02-01 00:19:11 +0100

    lexer

--- srcs/lexer/add_token.c
+++ srcs/lexer/add_token.c
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 17:32:32 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/01/31 17:33:45 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/02/01 00:17:52 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 void	add_token(char **tokens, int *size, char *token, int *token_len)
 {
-	if (*token_len > 0)
-	{
-		token[*token_len] = '\0';
-		(*size)++;
-		*tokens = realloc(*tokens, sizeof(char *) * (*size + 1));
-		(*tokens)[*size - 1] =ft_strdup(token);
-		*token_len = 0;
-	}
+	if (*token_len == 0)
+		return ;
+	token[*token_len] = '\0';
+	(*size)++;
+	*tokens = realloc(*tokens, sizeof(char *) * (*size + 1));
+	(*tokens)[*size - 1] =ft_strdup(token);
+	*token_len = 0;
 }
--- srcs/lexer/tokenize_input.c
+++ srcs/lexer/tokenize_input.c
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 20:41:29 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/01/31 17:32:26 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/02/01 00:19:05 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,56 @@
 
 static bool	is_separator(char c)
 {
-    return (c == ' ' || c == '|' || c == '>' || c == '<' || c == '&');
+    return (c == ' ' || c == '|' || c == '>' || c == '<' || c == '&' \
+	|| c == '(' || c == ')');
 }
 
 char	**tokenize_input(const char *input)
 {
 	char	**tokens;
+	char	c;
 	char	token[MAX_TOKEN_LEN];
-	int		token_len;
 	int		size;
+	int		token_len;
 	bool	in_quote;
 	int		i;
 
+	tokens = NULL;
 	size = 0;
 	token_len = 0;
-	i = 0;
 	in_quote = false;
-	token[0] = NULL;
+	i = 0;
 	tokens = malloc(sizeof(char *));
-	if (tokens == NULL)
+	if (!tokens)
 		return (NULL);
-	while (input[i] != '\0')
+    tokens[0] = NULL;
+    while (input[i] != '\0') 
 	{
-		if (is_serapator(input[i] != '\0') && !in_quote)
-			add_token(&tokens, &size, token, &token_len);
-		else
-			handle_char(input[i], token, &token_len, &in_quote);
-	}
-	add_token(&tokens, &size, token, &token_len);
-	tokens[size] = NULL;
-	return (tokens);
+        c = input[i];
+        if ((c == '\"' || c == '\'') && !in_quote)
+		{
+            in_quote = true;
+            continue;
+		}
+        else if ((c == '\"' || c == '\'') && in_quote) 
+		{
+            in_quote = false;
+            continue;
+        }
+        if (!in_quote && is_special_char(c))
+		{
+            add_token(&tokens, &size, token, &token_len);
+            token[0] = c;
+            token[1] = '\0';
+            add_token(&tokens, &size, token, &token_len);
+		}
+        else if (!in_quote && is_separator(c))
+            add_token(&tokens, &size, token, &token_len);
+        else
+            token[token_len++] = c;
+		i++;
+    }
+    add_token(&tokens, &size, token, &token_len);
+    tokens[size] = NULL;
+    return (tokens);
 }
